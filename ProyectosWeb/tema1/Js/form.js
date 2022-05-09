ac3af1e9fbc28dc5433ff4c3ea389e264cc884df
Dateneo <dateneo@gmail.com>
2022-05-08 23:56:00 -0500

    first commit

---
+++ ProyectosWeb/tema1/Js/form.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-boton').addEventListener('click',() =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.botones'), {delay: 500};
+ScrollReveal().reveal('.Publicidad'), {delay: 500};
+ScrollReveal().reveal('.Publicidad2'), {delay: 500};
+ScrollReveal().reveal('.redes'), {delay: 500};
+ScrollReveal().reveal('.links'), {delay: 500};
+ScrollReveal().reveal('.Pagina-Links'), {delay: 500};
+ScrollReveal().reveal('.pie'), {delay: 500};
